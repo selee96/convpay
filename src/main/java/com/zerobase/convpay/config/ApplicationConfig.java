43cf11b17862cde15b3855d799489152f0e03f2c
selee96 <pictur96@naver.com>
2024-07-09 00:47:17 +0900

    resource 사용법

--- src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
+++ src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -2,10 +2,14 @@ package com.zerobase.convpay.config;
 
 import com.zerobase.convpay.ConvpayApplication;
 import com.zerobase.convpay.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -14,6 +18,14 @@ import java.util.HashSet;
 // @ComponentScan(basePackages = "com.zerobase.convpay")
 @ComponentScan(basePackageClasses = ConvpayApplication.class)
 public class ApplicationConfig {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    public void getResource() throws IOException {
+        Resource resource = applicationContext.getResource("myTemplete.txt");
+        System.out.println(resource.contentLength() + "");
+    }
 
     /* 3. JavaConfig 를 통한 Bean 등록
     @Bean
@@ -46,7 +58,4 @@ public class ApplicationConfig {
         return new DiscountByPaymethod();
     }
     */
-
-
-
 }
